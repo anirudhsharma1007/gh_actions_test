b34f8cc93dad5f6c26f9b4c5b6a071ca0f1586b8
anirudhsharma1007 <32289850+anirudhsharma1007@users.noreply.github.com>
2023-06-20 19:33:30 -0400

    made changes to folder path reading for ghActions

--- data/readArabicToHtml.py
+++ data/readArabicToHtml.py
@@ -2,7 +2,7 @@
 import os
 import re
 
-filesFolderPath = os.getcwd() + '\\data'
+filesFolderPath = os.path.join(os.getcwd(),'data')
 
 # def escape_special_characters(string):
 #     special_characters = r"[!\"#$%&'()*+,\-.\/:;<=>?@\[\\\]^_`{|}~]"
@@ -29,7 +29,8 @@ def generate_file_list_html(folder_path):
     file_names = []
     for file_name in os.listdir(folder_path):
         if os.path.isfile(os.path.join(folder_path, file_name)):
-            file_names.append(file_name)
+            if str(file_name).split('.')[-1] != 'html':
+                file_names.append(file_name)
 
     html_content = "<html>\n<body>\n<ul>\n"
     for file_name in file_names:
@@ -50,7 +51,7 @@ def create_html_content(file_name):
         data = read_file.read()
         html_content += data
     html_content += "\n</body>\n</html>"
-    html_content = re.sub(r"###\s*\| (.+)", r"<h1>\1</h1>", html_content)
+    html_content = re.sub(r"### |\s*\ (.+)", r"<h4>\1</h4>", html_content)
     print(file_name.split('.')[0])
     with open(file_name.split('.')[0] + '.html','w',encoding='utf-8') as html_file:        
         html_file.write(html_content)
@@ -71,15 +72,25 @@ def create_html_content_with_h2_tag(file_name):
         if line.startswith("#META#"):
             meta_line = line.lstrip("#META#").strip()
             if arabic_h2 and re.search("[\u0600-\u06FF]", meta_line):
-                html_content += "<h2>" + meta_line + "</h2>"
+                html_content += "<h1>" + meta_line + "</h1>"
                 arabic_h2 = False
             elif english_h3 and re.search("[A-Za-z]", meta_line):
-                html_content += "<h3>" + meta_line + "</h3>"
+                html_content += "<h2>" + meta_line + "</h2>"
                 english_h3 = False
+        elif line.startswith("### |||"):
+            h6_line = line.lstrip("### |||").strip()
+            html_content += "<h5>" + h6_line + "</h5>"
+        elif line.startswith("### ||"):
+            h5_line = line.lstrip("### ||").strip()
+            html_content += "<h4>" + h5_line + "</h4>"
+        elif line.startswith("### |"):
+            h4_line = line.lstrip("### |").strip()
+            html_content += "<h3>" + h4_line + "</h3>"
         else:
             html_content += line
-
-    html_content += "\n</body>\n</html>"
+    
+    # html_content = re.sub(r"### |\s*\ (.+)", r"<h4>\1</h4>", html_content)
+    html_content += "</p>\n</body>\n</html>"
 
     with open(file_name.split('.')[0] + '.html', 'w', encoding='utf-8') as html_file:
         html_file.write(html_content)
