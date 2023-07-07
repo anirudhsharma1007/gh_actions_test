ea28cfe864fa8745a47cbb15ac8fa6e15fecaa87
anirudhsharma1007 <32289850+anirudhsharma1007@users.noreply.github.com>
2023-07-07 16:14:30 -0400

    Add files via upload

--- .github/build_html.py
+++ .github/build_html.py
@@ -5,6 +5,16 @@ import sys
 
 import pandas as pd
 
+
+df = pd.read_excel(os.path.join(os.getcwd(),'Witness_list_sheet.xlsx'))
+selected_columns = df[['Arabic name', 'Witness ']]
+witness_dict = {}
+for index, row in selected_columns.iterrows():
+    key = row['Witness ']
+    value = str(row['Arabic name'])
+    witness_dict[key] = value
+
+
 df = pd.read_excel(os.path.join(os.getcwd(),'Kevin Bibliography.xlsx'))
 selected_columns = df[['ID', 'short_author', 'short_title']]
 my_dict = {}
@@ -133,6 +143,7 @@ def create_html_content_with_h2_tag(file_name):
     citation_content = ''
 
     for line in lines:
+        line = re.sub(r"PageV(\d+)P(\d+)", r"<span title='vol \1 page \2'>§</span>", line)
         if line.startswith("#META#"):
             meta_line = line.lstrip("#META#").strip()
             if arabic_h2 and re.search("[\u0600-\u06FF]", meta_line):
@@ -142,8 +153,38 @@ def create_html_content_with_h2_tag(file_name):
                 html_content += "<h2>" + meta_line + "</h2>"
                 english_h3 = False
         elif line.startswith("# @COMMENT:"):
-            comment_text = line.lstrip("# @COMMENT:").strip()
-            html_content += f"<button onclick=\"alert('{comment_text}')\">Comment</button>"
+            # comment_lines = line.lstrip("# @COMMENT:").strip()
+            comment_lines = line.replace("# @COMMENT:", "").strip()
+            comment_text = ''
+            words = comment_lines.strip().split()
+            for word in words:
+                if word.startswith("#$"):
+                    block_name = word[2:]
+                    key = block_name[:4] 
+                    remaining_tag = block_name[4:]
+                    if key in my_dict:
+                        value = my_dict[key]  # Get the corresponding value from the dictionary
+
+                        # Replace the prefix pattern with 'volume <number>'
+                        remaining_tag = re.sub(patterns['prefix_pattern'], r' vol. \1', remaining_tag)
+
+                        # Replace the suffix pattern with 'page number <number><alphabet>'
+                        remaining_tag = re.sub(patterns['suffix_pattern'], r'. \1\2', remaining_tag)
+
+                        comment_text += value + remaining_tag + ' '
+                    else:
+                        comment_text += block_name + ' '
+                elif word.startswith("wit_"):
+                    key = word[4:9] 
+                    if key in witness_dict:
+                        value = witness_dict[key]  # Get the corresponding value from the dictionary
+
+                        comment_text += value + ' '
+                    else:
+                        comment_text += word + ' '
+                else:
+                     comment_text += word + ' '
+            html_content += f"<br><button onclick=\"alert('{comment_text}')\">Comment</button><br>"
         elif re.search(r"# @[A-Za-z0-9_]+_BEG_", line):
             block_name = re.search(r"# @([A-Za-z0-9_]+)_BEG_", line).group(1)
             key = block_name[:4] 
@@ -169,24 +210,38 @@ def create_html_content_with_h2_tag(file_name):
             for word in words:
                 # bidi_word = get_display(arabic_reshaper.reshape(word))
                 # html_content += bidi_word + ' '
+                if word.startswith("@TR"):
+                    continue
                 if re.search(r"@"+block_name+"_END_", word):
                     citation_block = False
-                    html_content += f"<button onclick=\"alert('{citation_content}');\">Citation ({block_name})</button>"
+                    html_content += f"<br><button onclick=\"alert('{citation_content}');\">Citation ({block_name})</button><br>"
                     break
                 elif re.search(r"@"+block_name+"_BEG_", word):
                     continue
+                elif re.search(r"@"+block_name+"_END_", word):
+                    continue
                 else:
-                    html_content += word + ' '
+                    if word != '#':
+                        html_content += word + ' '
         
         elif citation_block:
+            if re.search(r"@"+block_name+"_END_", word):
+                    citation_block = False
+                    html_content += f"<br><button onclick=\"alert('{citation_content}');\">Citation ({block_name})</button><br>"
+                    
             words = line.strip().split()
             for word in words:
                 # bidi_word = get_display(arabic_reshaper.reshape(word))
                 # html_content += bidi_word + ' '
-                html_content += word + ' '
-                if re.search(r"# @[A-Za-z0-9_]+_END_", word):
+                if word.startswith("@TR"):
+                    continue
+                if re.search(r"@"+block_name+"_END_", word):
                     citation_block = False
-                    html_content += f"<button onclick=\"displayCitation('{citation_content}');\">Citation ({block_name})</button>"
+                    html_content += f"<br><button onclick=\"alert('{citation_content}');\">Citation ({block_name})</button><br>"
+                else:
+                    if word != '#':
+                        html_content += word + ' '
+
         elif line.startswith("### |||"):
             h6_line = line.lstrip("### |||").strip()
             html_content += "<h5>" + h6_line + "</h5>"
